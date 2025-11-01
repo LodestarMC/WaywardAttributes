3cf10aee20f4301acb77a120eac68243ea2514a5
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2025-11-01 19:50:04 +0100

    Update AttributeTooltipRenderer.java

--- src/main/java/team/lodestar/wayward_attributes/client/AttributeTooltipRenderer.java
+++ src/main/java/team/lodestar/wayward_attributes/client/AttributeTooltipRenderer.java
@@ -41,7 +41,6 @@ public class AttributeTooltipRenderer {
                     int color = textColor != null ? textColor.getValue() : -1;
                     component = new AttributeTooltipComponent(display, textTooltip.text, color);
                 }
-                //
             }
             mutable.set(i, component);
         }
