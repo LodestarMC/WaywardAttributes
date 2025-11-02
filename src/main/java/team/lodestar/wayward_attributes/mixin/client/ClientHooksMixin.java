c3d11ec79372646dfa6396a1f8a57b34c8a86e08
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2025-11-02 08:28:59 +0100

    Update ClientHooksMixin.java

--- src/main/java/team/lodestar/wayward_attributes/mixin/client/ClientHooksMixin.java
+++ src/main/java/team/lodestar/wayward_attributes/mixin/client/ClientHooksMixin.java
@@ -1,5 +1,6 @@
 package team.lodestar.wayward_attributes.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import com.llamalad7.mixinextras.sugar.*;
 import com.mojang.datafixers.util.*;
 import net.minecraft.client.gui.*;
@@ -19,7 +20,7 @@ import java.util.*;
 @Mixin(ClientHooks.class)
 public class ClientHooksMixin {
 
-    @Inject(method = "gatherTooltipComponentsFromElements", at = @At(value = "RETURN"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/client/event/RenderTooltipEvent$GatherComponents;getMaxWidth()I")), cancellable = true)
+    @WrapOperation(method = "gatherTooltipComponentsFromElements", at = @At(value = "RETURN"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/client/event/RenderTooltipEvent$GatherComponents;getMaxWidth()I")))
     private static void waywardAttributes$addEnchantmentTooltip(ItemStack stack, List<Either<FormattedText, TooltipComponent>> elements, int mouseX, int screenWidth, int screenHeight, Font fallbackFont, CallbackInfoReturnable<List<ClientTooltipComponent>> cir, @Local(name = "font") Font font, @Local RenderTooltipEvent.GatherComponents event) {
         cir.setReturnValue(AttributeTooltipRenderer.addToTooltip(stack, elements, cir.getReturnValue(), mouseX, screenWidth, font, event));
     }
